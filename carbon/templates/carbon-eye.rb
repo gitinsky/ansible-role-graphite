0c19e435a8ee1df3112d1e986faa209e50b3a4ed
Alex Chistyakov <alexclear@gmail.com>
2016-05-30 19:40:13 +0300

    This was needed to start on Ubuntu 16.04

--- carbon/templates/carbon-eye.rb
+++ carbon/templates/carbon-eye.rb
@@ -3,10 +3,12 @@ Eye.application 'carbon' do
   stdall '/var/log/eye/carbon-stdall.log' # stdout,err logs for processes by default
   trigger :flapping, times: 10, within: 1.minute, retry_in: 3.minutes
   check :cpu, every: 10.seconds, below: 100, times: 3 # global check for all processes
+  env 'GRAPHITE_ROOT' => '/opt/graphite'
+  env 'PYTHONPATH' => '/usr/local/lib/python2.7/dist-packages/opt/graphite/lib'
 
   process :carbon do
     pid_file '/var/run/carbon-eye.pid'
-    start_command 'carbon-cache.py --debug start'
+    start_command 'carbon-cache.py --config /opt/graphite/conf/carbon.conf --debug start'
 
     daemonize true
     start_timeout 10.seconds
